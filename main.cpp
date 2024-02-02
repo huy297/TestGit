187c57f875abf4e60e8ae32a3d67c1b870cf18de
huy297 <duchuy2972005@gmail.com>
2024-02-03 02:04:34 +0700

    init project

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("Simple Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Set the draw color to blue (0, 0, 255, 255)
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    // Create a rectangle for the player
+    SDL_Rect playerRect = {400, 300, 50, 50};
+
+    // Event loop
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        // Handle events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            } else if (e.type == SDL_KEYDOWN) {
+                // Handle key presses for player movement
+                switch (e.key.keysym.sym) {
+                    case SDLK_UP:
+                        playerRect.y -= 10;
+                        break;
+                    case SDLK_DOWN:
+                        playerRect.y += 10;
+                        break;
+                    case SDLK_LEFT:
+                        playerRect.x -= 10;
+                        break;
+                    case SDLK_RIGHT:
+                        playerRect.x += 10;
+                        break;
+                }
+            }
+        }
+
+        // Clear the screen (black)
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Render the player rectangle
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderFillRect(renderer, &playerRect);
+
+        // Update the screen
+        SDL_RenderPresent(renderer);
+
+        // Add a short delay to control the frame rate
+        SDL_Delay(16);
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
